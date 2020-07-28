b6d104b7417af226425f90e3569d279e97de069f
juancoob <ja.cobos.obrero@gmail.com>
2020-07-28 18:17:52 +0200

    feat: Update constant file data for the image compression

--- app/src/main/java/com/example/fileonfire/util/Constants.kt
+++ app/src/main/java/com/example/fileonfire/util/Constants.kt
@@ -7,3 +7,11 @@ const val CHOOSE_IMAGE_REQUEST = 1
 const val EOF = -1
 const val ONE_MB = 1000
 const val MAX_NUMBER = 999_950
+const val DEFAULT_QUALITY = 80
+
+// Default file measures
+const val DEFAULT_WIDTH = 612
+const val DEFAULT_HEIGHT = 816
+
+// MYME TYPE
+const val WEBP_MIME_TYPE = "webp"
